bc46837d62921e850015bc1d3a77406f3fe3d8b3
Raheem Kawojue <kawojue08@gmail.com>
2023-09-16 21:50:51 +0100

    [edit] q3

--- Q3/README.md
+++ Q3/README.md
@@ -1,11 +1,12 @@
-# Q3
+# Q3 - Paint Estimate
 
 A painting company has determined that for every 112 square feet of wall space, one gallon of paint
 is required. The company charges $25.00 per hour for labour. Write a program that asks the user to enter the number of square feet of wall space to be painted and the price of the paint per gallon.
 The program should then display the following data.
-• The number of gallons of paint required
-• The cost of the paint
-• The hours of labour required to complete the job
-• The cost of the labour to complete the job
-• The total cost of the paint job.
+
+1. The number of gallons of paint required
+2. The cost of the paint
+3. The hours of labour required to complete the job
+4. The cost of the labour to complete the job
+5. The total cost of the paint job.
 Assume that the program will run only once for each estimate.
--- Q3/q3.py
+++ Q3/q3.py
@@ -1,30 +1,18 @@
-def main():
-    # Get user input
-    square_feet = int(input("Enter the number of square feet of wall space to be painted: "))
-    paint_price_per_gallon = float(input("Enter the price of paint per gallon: "))
+square_feet = float(input("Enter the number of square feet of wall space to be painted: "))
+paint_price = float(input("Enter the price of paint per gallon: $"))
 
-    # Calculate the number of gallons of paint required
-    gallons_required = square_feet / 112
+gallons_required = square_feet / 112
 
-    # Calculate the cost of the paint
-    paint_cost = gallons_required * paint_price_per_gallon
+paint_cost = gallons_required * paint_price
 
-    # Calculate the hours of labor required to complete the job
-    hours_of_labor = square_feet / 112
+labor_hours = square_feet / 112
 
-    # Calculate the cost of labor to complete the job
-    labor_cost = hours_of_labor * 25.00
+labor_cost = labor_hours * 25.00
 
-    # Calculate the total cost of the paint job
-    total_cost = paint_cost + labor_cost
+total_cost = paint_cost + labor_cost
 
-    # Display the results
-    print(f"\nNumber of gallons of paint required: {gallons_required:.2f}")
-    print(f"Cost of paint: ${paint_cost:.2f}")
-    print(f"Hours of labor required: {hours_of_labor:.2f}")
-    print(f"Cost of labor: ${labor_cost:.2f}")
-    print(f"Total cost of the paint job: ${total_cost:.2f}")
-
-
-if __name__ == "__main__":
-    main()
+print(f"The number of gallons of paint required: {gallons_required:.2f} gallons")
+print(f"The cost of the paint: ${paint_cost:.2f}")
+print(f"The hours of labor required to complete the job: {labor_hours:.2f} hours")
+print(f"The cost of the labor to complete the job: ${labor_cost:.2f}")
+print(f"The total cost of the paint job: ${total_cost:.2f}")
