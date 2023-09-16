75068df18267edcd148cf8be0d4cbb3ff55a1c2e
Raheem Kawojue <kawojue08@gmail.com>
2023-09-16 21:50:43 +0100

    [edit] q2

--- Q2/README.md
+++ Q2/README.md
@@ -1,3 +1,3 @@
-# Q2
+# Q2 - Automobie Expenses
 
 Write a program that asks the user to enter the monthly costs for the following expenses incurred from operating his or her automobile: loan payment, insurance, gas, and maintenance. The program should then display the total monthly costs for these expenses and the total annual costs for these expenses.
--- Q2/q2.py
+++ Q2/q2.py
@@ -1,32 +1,10 @@
-def get_monthly_costs():
-    loan_payment = float(input("Enter the monthly loan payment: "))
-    insurance = float(input("Enter the monthly insurance cost: "))
-    gas = float(input("Enter the monthly gas cost: "))
-    maintenance = float(input("Enter the monthly maintenance cost: "))
-    return loan_payment, insurance, gas, maintenance
+loan_payment = float(input("Enter your monthly loan payment: $"))
+insurance = float(input("Enter your monthly insurance cost: $"))
+gas = float(input("Enter your monthly gas cost: $"))
+maintenance = float(input("Enter your monthly maintenance cost: $"))
 
+total_monthly_costs = loan_payment + insurance + gas + maintenance
+total_annual_costs = total_monthly_costs * 12
 
-def calculate_total_monthly_costs(loan_payment, insurance, gas, maintenance):
-    total_monthly_costs = loan_payment + insurance + gas + maintenance
-    return total_monthly_costs
-
-
-def calculate_total_annual_costs(total_monthly_costs):
-    total_annual_costs = total_monthly_costs * 12
-    return total_annual_costs
-
-
-def main():
-    print("Welcome to the Automobile Expense Calculator!")
-    print("Please enter the monthly costs for the following expenses:")
-
-    loan_payment, insurance, gas, maintenance = get_monthly_costs()
-    total_monthly_costs = calculate_total_monthly_costs(loan_payment, insurance, gas, maintenance)
-    total_annual_costs = calculate_total_annual_costs(total_monthly_costs)
-
-    print("\nTotal Monthly Costs: ${:.2f}".format(total_monthly_costs))
-    print("Total Annual Costs: ${:.2f}".format(total_annual_costs))
-
-
-if __name__ == "__main__":
-    main()
+print("\nTotal Monthly Expenses: $", total_monthly_costs)
+print("Total Annual Expenses: $", total_annual_costs)
