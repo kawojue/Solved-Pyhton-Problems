26d6eba77fc04c62860ea1d0ab3e327f9958f268
Raheem Kawojue <kawojue08@gmail.com>
2023-09-16 21:50:36 +0100

    [edit] q1

--- Q1/README.md
+++ Q1/README.md
@@ -1,9 +1,10 @@
-# Q1
+# Q1 - HotDog
 
 Assume hot dogs come in packages of 10, and hotdog buns come in packages of 8. Write a program that calculates the number of packages of hot dogs and the number of packages of hot dog buns needed for a cookout, with minimum amount of leftovers. The program should ask the user for the number of people attending the cookout and the number of hot dogs each person will be given.
 
 The program should display the following details:
-• The minimum number of packages of hot dogs required.
-• The minimum number of packages of hot dog buns required
-• The number of hot dogs that will be left over
-• The number of hot dog buns that will be left over
+
+1. The minimum number of packages of hot dogs required.
+2. The minimum number of packages of hot dog buns required
+3. The number of hot dogs that will be left over
+4. The number of hot dog buns that will be left over
--- Q1/q1.py
+++ Q1/q1.py
@@ -1,34 +1,21 @@
-def calculate_packages(num_people, hotdogs_per_person):
-    hotdogs_per_package = 10
-    buns_per_package = 8
+HOT_DOGS_PER_PACKAGE = 10
+BUNS_PER_PACKAGE = 8
 
-    total_hotdogs_needed = num_people * hotdogs_per_person
-    hotdog_packages_needed = (total_hotdogs_needed + hotdogs_per_package - 1) // hotdogs_per_package
+num_people = int(input("Enter the number of people attending the cookout: "))
+hotdogs_per_person = int(input("Enter the number of hot dogs each person will be given: "))
 
-    total_buns_needed = num_people * hotdogs_per_person
-    buns_packages_needed = (total_buns_needed + buns_per_package - 1) // buns_per_package
+print()
 
-    hotdogs_leftover = (hotdog_packages_needed * hotdogs_per_package) - total_hotdogs_needed
-    buns_leftover = (buns_packages_needed * buns_per_package) - total_buns_needed
+total_hotdogs_needed = num_people * hotdogs_per_person
+total_buns_needed = total_hotdogs_needed // HOT_DOGS_PER_PACKAGE * BUNS_PER_PACKAGE
 
-    return hotdog_packages_needed, buns_packages_needed, hotdogs_leftover, buns_leftover
+hotdogs_leftover = total_hotdogs_needed % HOT_DOGS_PER_PACKAGE
+buns_leftover = total_buns_needed % BUNS_PER_PACKAGE
 
+packages_hotdogs_required = total_hotdogs_needed // HOT_DOGS_PER_PACKAGE
+packages_buns_required = total_buns_needed // BUNS_PER_PACKAGE
 
-def main():
-    try:
-        num_people = int(input("Enter the number of people attending the cookout: "))
-        hotdogs_per_person = int(input("Enter the number of hot dogs each person will be given: "))
-
-        hotdog_packages_needed, buns_packages_needed, hotdogs_leftover, buns_leftover = calculate_packages(num_people, hotdogs_per_person)
-
-        print("\nMinimum number of packages of hot dogs required:", hotdog_packages_needed)
-        print("Minimum number of packages of hot dog buns required:", buns_packages_needed)
-        print("Number of hot dogs left over:", hotdogs_leftover)
-        print("Number of hot dog buns left over:", buns_leftover)
-
-    except ValueError:
-        print("Invalid input. Please enter a valid number.")
-
-
-if __name__ == "__main__":
-    main()
+print("Minimum number of packages of hot dogs required: ", packages_hotdogs_required)
+print("Minimum number of packages of hot dog buns required: ", packages_buns_required)
+print("Number of hot dogs left over: ", hotdogs_leftover)
+print("Number of hot dog buns left over: ", buns_leftover)
